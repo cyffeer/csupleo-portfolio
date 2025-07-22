c48e05d7a818aa682a50872c2e1c6a5ff0e97f1b
cyffeer <130665260+cyffeer@users.noreply.github.com>
2025-07-22 18:34:03 +0800

    fix import paths in App.jsx to include file extensions

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Navbar from "./Components/Navbar/NavBar.jsx";
-import Hero from "./Components/Hero/Hero";
-import About from "./Components/About/About";
-import Internship from "./Components/Services/Internship";
-import Education from "./Components/Services/Education";
-import Experience from "./Components/Services/Experience";
-import MyProject from "./Components/MyWork/MyProject";
-import Contact from "./Components/Contact/Contact";
-import Footer from "./Components/Footer/Footer";
-import MyAward from "./Components/MyWork/MyAward";
-import MyCertification from "./Components/MyWork/MyCertification";
+import Hero from "./Components/Hero/Hero.jsx";
+import About from "./Components/About/About.jsx";
+import Internship from "./Components/Services/Internship.jsx";
+import Education from "./Components/Services/Education.jsx";
+import Experience from "./Components/Services/Experience.jsx";
+import MyProject from "./Components/MyWork/MyProject.jsx";
+import Contact from "./Components/Contact/Contact.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
+import MyAward from "./Components/MyWork/MyAward.jsx";
+import MyCertification from "./Components/MyWork/MyCertification.jsx";
 
 //hello
 const App = () => {
